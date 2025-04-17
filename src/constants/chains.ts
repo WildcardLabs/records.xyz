35b44526b5599af7813c0de3e4937a8e595abf1b
stevegachau.eth <steve@zawadi.co>
2025-04-17 13:01:57 +0100

    Update chains.ts

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -5,5 +5,6 @@ export const chainLogos = {
   base: "/lovable-uploads/704c1ede-df6b-4911-af43-2d274d033df6.png",
   arbitrum: "/lovable-uploads/ecaff6c4-37dd-4440-b585-dcf98a2440cf.png",
   linea: "/lovable-uploads/b871bebc-5864-440b-b642-101a2728678f.png",
-  polygon: "/lovable-uploads/70e574c2-77c3-4ced-a446-d98951501fb1.png"
+  polygon: "/lovable-uploads/70e574c2-77c3-4ced-a446-d98951501fb1.png",
+  linea: "/lovable-uploads/linea.png"
 } as const;
